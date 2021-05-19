c69ca82a95b59f0725e037fc873f5bc0a9fe4958
bbaktaeho <lth9036@gmail.com>
2021-05-19 19:20:50 +0900

    Update: 개체 주석 추가

--- src/common/interfases/comment.interface.ts
+++ src/common/interfases/comment.interface.ts
@@ -1,5 +1,9 @@
 import { IModel } from "./model.interface";
 
+/**
+ * 댓글
+ * @property {body} 댓글 내용
+ */
 export interface IComment extends IModel {
   body: string;
 }
--- src/common/interfases/user.interface.ts
+++ src/common/interfases/user.interface.ts
@@ -1,5 +1,11 @@
 import { IModel } from "./model.interface";
 
+/**
+ * 사용자
+ * @property {email} 사용자 이메일
+ * @property {nickname} 사용자 닉네임
+ * @property {password} 사용자 비밀번호
+ */
 export interface IUser extends IModel {
   email: string;
   nickname: string;
